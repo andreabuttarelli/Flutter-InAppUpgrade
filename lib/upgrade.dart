1d7e3c7c10b1644b603bfeaee4288826329f2cfc
andreabuttarelli <andreabuttarelli.design@gmail.com>
2023-08-20 18:14:06 +0200

    Update upgrade.dart

--- lib/upgrade.dart
+++ lib/upgrade.dart
@@ -1,10 +1,12 @@
 library upgrade;
 
-export 'core/upgrade_manager.dart';
-export 'core/installer.dart';
+export 'package:version/version.dart';
+
 export 'core/custom_upgrade_dialog.dart';
 export 'core/custom_upgrade_status_indicator.dart';
 export 'core/custom_upgrade_view.dart';
+export 'core/installer.dart';
+export 'core/upgrade_manager.dart';
 export 'core/upgrade_state_change_notifier.dart';
 export 'installer/android_apk_installer.dart';
 export 'installer/android_app_market_installer.dart';
@@ -18,4 +20,3 @@ export 'models/appcast_item.dart';
 export 'models/upgrade_status.dart';
 export 'utils/current_version_manager.dart';
 export 'utils/default_file_location.dart';
-export 'package:version/version.dart';
